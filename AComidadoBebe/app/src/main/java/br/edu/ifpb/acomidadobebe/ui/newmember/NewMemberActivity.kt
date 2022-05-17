29462a5da9459501d1573defffa70371ceea47a1
CleoniceSoares <cleonice.soares27cs@gmail.com>
2022-05-17 15:09:11 -0300

    create new-member

---
+++ AComidadoBebe/app/src/main/java/br/edu/ifpb/acomidadobebe/ui/newmember/NewMemberActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifpb.acomidadobebe.ui.newmember
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifpb.acomidadobebe.R
+
+class NewMemberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_member)
+    }
+}
