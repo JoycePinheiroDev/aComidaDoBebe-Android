f2d8e8e4d9e43fc2404a0a2aaea18195e61c0e6b
danielccosta9 <danielc.costa9@gmail.com>
2022-07-11 21:02:23 -0300

    Home Menu

---
+++ AComidadoBebe/app/src/main/java/br/edu/ifpb/acomidadobebe/ui/menu/MenuFragment.kt
@@ -0,0 +1,35 @@
+package br.edu.ifpb.acomidadobebe.ui.menu
+
+import android.arch.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.edu.ifpb.acomidadobebe.R
+
+class MenuFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MenuFragment()
+    }
+
+    private lateinit var viewModel: MenuViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.menu_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(MenuViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ AComidadoBebe/app/src/main/java/br/edu/ifpb/acomidadobebe/ui/menu/MenuViewModel.kt
@@ -0,0 +1,7 @@
+package br.edu.ifpb.acomidadobebe.ui.menu
+
+import android.arch.lifecycle.ViewModel
+
+class MenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
