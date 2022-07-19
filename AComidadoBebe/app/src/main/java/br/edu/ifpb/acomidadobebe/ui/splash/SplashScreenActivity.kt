4525e4a42d83743241fca8f21a4e1267b32c148d
danielccosta9 <danielc.costa9@gmail.com>
2022-07-19 14:56:07 -0300

    Created splash screen activity.

--- AComidadoBebe/app/src/main/java/br/edu/ifpb/acomidadobebe/ui/splash/SplashScreenActivity.kt
+++ AComidadoBebe/app/src/main/java/br/edu/ifpb/acomidadobebe/ui/splash/SplashScreenActivity.kt
@@ -1,16 +1,38 @@
 package br.edu.ifpb.acomidadobebe.ui.splash
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.app.AppCompatActivity
 import br.edu.ifpb.acomidadobebe.databinding.ActivitySplashScreenBinding
+import br.edu.ifpb.acomidadobebe.ui.home.HomeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, DELAY)
+    }
+
+    companion object {
+        const val DELAY = 1000L
     }
 }
